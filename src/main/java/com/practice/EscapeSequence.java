cb99962dd50090a989564d27065a51c89ca9fe31
mrun1corn <robinisalegend01@gmail.com>
2024-10-21 13:24:02 +0600

    escape sequence

---
+++ src/main/java/com/practice/EscapeSequence.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        String l1 = "1st line";
+        String l2 = "2nd line";
+        System.out.print(l1 + "\n" + l2);
+    }
+
+}
