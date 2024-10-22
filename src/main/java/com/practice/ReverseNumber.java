c92ff4b13174d4260791d79ffcf07bcf22ed158d
mrun1corn <robinisalegend01@gmail.com>
2024-10-22 18:46:44 +0600

    pallindrom number

---
+++ src/main/java/com/practice/ReverseNumber.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum=0, r, temp;
+        System.out.print("Input a number: ");
+        int num = input.nextInt();
+        temp = num;
+        while(temp != 0){
+            r = temp % 10;
+            sum = sum * 10 + r;
+            temp = temp / 10;
+        }
+        if(num == sum){
+            System.out.println("number is pallindrom");
+        }
+        else{
+            System.out.println("not pallindrom");
+        }
+    }
+
+}
